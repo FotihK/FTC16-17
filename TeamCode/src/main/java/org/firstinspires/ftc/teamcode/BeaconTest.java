59d133d36425899504a6faa6141dc93deac62e63
Fotih Karimov <fotih.karimov@gmail.com>
2016-11-02 15:27:10 -0400

    Changed BeaconTest to use NXT Light Sensor rather than HT Color

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BeaconTest.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.LightSensor;
+
+/**
+ * Created by HP on 9/29/2016.
+ */
+
+@TeleOp(name="BeaconTest",group = "Tests")
+public class BeaconTest extends OpMode {
+    private LightSensor light;
+    @Override
+    public void init() {
+        light = hardwareMap.lightSensor.get("color");
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Light Value: ", light.getLightDetected());
+        telemetry.addData("Raw Light: ", light.getRawLightDetected());
+        telemetry.update();
+    }
+}
