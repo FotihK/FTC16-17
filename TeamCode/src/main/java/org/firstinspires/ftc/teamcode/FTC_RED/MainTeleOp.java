363996dc08075afb3970e469a332080514cbd99d
Fotih Karimov <fotih.karimov@gmail.com>
2016-12-10 00:21:46 -0500

    Comments and autonomous started

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_RED/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_RED/MainTeleOp.java
@@ -19,13 +19,14 @@ public class MainTeleOp extends OpMode {
     private DcMotor intake, belt, flywheelL, flywheelR;
     private Servo pushL, pushR;
     private boolean[] toggleStates = new boolean[5];    //0th is flywheel, 1st is intake, 2nd is belt, 3 is pushL, 4 is pushR
-    private boolean[] movingStates = new boolean[2];   //0th is intake, 1st is belt
+    private boolean[] movingStates = new boolean[2];    //0th is intake, 1st is belt
+    private boolean[] isOn = new boolean[3];            //0th is flywheel, 1st is intake, 2nd is belt
     private double flyPower = 0;
     private boolean flywheelOn = false;
     private ElapsedTime rampTimer = new ElapsedTime();
 
     @Override
-    public void init() {
+    public void init() {                                //Initializes hardware with directions and positions
         left = hardwareMap.dcMotor.get("left");
         right = hardwareMap.dcMotor.get("right");
         intake = hardwareMap.dcMotor.get("intake");
@@ -52,102 +53,123 @@ public class MainTeleOp extends OpMode {
 
     }
 
-    private double[] controllerYValues(Gamepad gamepad) {  // index 0 is left, index 1 is right
-        double[] returnVals = new double[2];
+    private double[] controllerYValues(Gamepad gamepad) {   // Returns controller values for passed gamepad
+        double[] returnVals = new double[2];                // index 0 is left, index 1 is right
         returnVals[0] = Math.abs(gamepad.left_stick_y) > 0.05 ? gamepad1.left_stick_y : 0;
         returnVals[1] = Math.abs(gamepad.right_stick_y) > 0.05 ? gamepad1.right_stick_y : 0;
 
         return returnVals;
     }
 
-    private void rampFlywheelUp(){
+    private void rampFlywheelUp(){                          //Ramp flywheel up, ΔP = 0.05, Δt = 115ms
         if(flyPower <= 0.95 && rampTimer.time() > 115) {
             flyPower += 0.05;
             flywheelL.setPower(flyPower);
             flywheelR.setPower(flyPower);
         }
-        if(flyPower == 0.95) flywheelOn = true;
+        if(flyPower == 0.95) {
+            flywheelOn = true;
+            isOn[0] = true;
+        }
         flywheelL.setPower(flyPower);
         flywheelR.setPower(flyPower);
     }
-    
-    private void rampFlywheelDown(){
+
+    private void rampFlywheelDown(){                        //Ramp flywheel down, ΔP = -0.05, Δt = 115ms
         if(flyPower > 0 && rampTimer.time() > 115){
             flyPower -= 0.05;
             flywheelL.setPower(flyPower);
             flywheelR.setPower(flyPower);
         }
-        if(flyPower == 0) flywheelOn = false;
+        if(flyPower == 0) {
+            flywheelOn = false;
+            isOn[0] = false;
+        }
         flywheelL.setPower(flyPower);
         flywheelR.setPower(flyPower);
+
     }
 
-    private void checkGamepad1(){
-        if(gamepad1.dpad_up && !toggleStates[1]){
+    private void checkGamepad1(){                       //Buttons for gamepad 1
+        if(gamepad1.dpad_up && !toggleStates[1]){       //Up toggle for intake
             if(movingStates[0]){
                 intake.setPower(0);
                 movingStates[0] = false;
+                isOn[1] = false;
             }
             else {
                 movingStates[0] = true;
                 intake.setPower(0.85);
+                isOn[1] = true;
             }
             toggleStates[1] = true;
         }
-        else if(gamepad1.dpad_down && !toggleStates[1]){
+        else if(gamepad1.dpad_down && !toggleStates[1]){    //Down toggle for intake
             if(movingStates[0]){
                 intake.setPower(0);
                 movingStates[0] = false;
+                isOn[1] = false;
             }
             else {
                 movingStates[0] = true;
                 intake.setPower(-0.85);
+                isOn[1] = true;
             }
             toggleStates[1] = true;
         }
         else if(!gamepad1.dpad_down && !gamepad1.dpad_up) toggleStates[1] = false;
     }
 
-    private void checkGamepad2(){
-        if(gamepad2.dpad_up && !toggleStates[2]){
+    private void checkGamepad2(){                   //Buttons for gamepad 2
+        if(gamepad2.dpad_up && !toggleStates[2]){   //Up toggle for belt
             if(movingStates[1]){
                 belt.setPower(0);
                 movingStates[1] = false;
+                isOn[2] = false;
             }
             else {
                 movingStates[1] = true;
                 belt.setPower(0.85);
+                isOn[2] = true;
             }
             toggleStates[2] = true;
         }
-        else if(gamepad2.dpad_down && !toggleStates[2]){
+        else if(gamepad2.dpad_down && !toggleStates[2]){    //Down toggle for belt
             if(movingStates[1]){
                 belt.setPower(0);
                 movingStates[1] = false;
+                isOn[2] = false;
             }
             else {
                 movingStates[1] = true;
                 belt.setPower(-0.85);
+                isOn[2] = true;
             }
             toggleStates[2] = true;
         }
         else if(!gamepad2.dpad_down && !gamepad2.dpad_up) toggleStates[2] = false;
 
-        if(gamepad2.y && !toggleStates[0]){
+        if(gamepad2.y && !toggleStates[0]){             //Toggle for flywheel
             if(flywheelOn) rampFlywheelDown();
             else rampFlywheelUp();
             toggleStates[0] = true;
         } else if(!gamepad2.y) toggleStates[0] = false;
 
-        if(gamepad2.left_bumper && !toggleStates[3]){
+        if(gamepad2.left_bumper && !toggleStates[3]){   //Toggle for pushL
             pushL.setPosition(pushL.getPosition() == 0.04 ? 0.9 : 0.04);
             toggleStates[3] = true;
         } else if(!gamepad2.left_bumper) toggleStates[3] = false;
 
-        if(gamepad2.right_bumper && !toggleStates[4]){
+        if(gamepad2.right_bumper && !toggleStates[4]){  //Toggle for pushR
             pushR.setPosition(pushR.getPosition() == 0.04 ? 0.9 : 0.04);
             toggleStates[4] = true;
         } else if(!gamepad2.right_bumper) toggleStates[4] = false;
+    }
+
+    private void telemetry(){
+        telemetry.addData("Flywheel is ", (isOn[0] ? "on. " : "off."));
+        telemetry.addData("Intake is ", (isOn[1] ? "on. " : "off."));
+        telemetry.addData("Belt is ", (isOn[2] ? "on. " : "off."));
     }
 
     @Override
@@ -157,6 +179,8 @@ public class MainTeleOp extends OpMode {
 
         checkGamepad1();
         checkGamepad2();
+
+        telemetry();
     }
 
 
